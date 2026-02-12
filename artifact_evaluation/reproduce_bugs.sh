70bb491dec627ac9eaf8f8be19af4037e10ba9d3
Sebastião Amaro <sebastiao.amaro@tecnico.ulisboa.pt>
2026-02-12 06:59:57 +0000

    updated scripts

--- artifact_evaluation/reproduce_bugs.sh
+++ artifact_evaluation/reproduce_bugs.sh
@@ -4,13 +4,11 @@ vagrant up test1
 vagrant ssh test1 -c "cd /vagrant/artifact_evaluation/bug_reproduction/ && python3 run.py scf_bugs.txt"
 
 #ZK 4203 only shows in scenarios where the cpu is overloaded, we emulate this by reducing the number of cores
-cd ../auxiliary_scripts/
-./change_cores.sh 1
+cd ../auxiliary_scripts/ && ./change_cores.sh 1 && cd ..
 vagrant reload test1
 vagrant ssh test1 -c "cd /vagrant/artifact_evaluation/bug_reproduction/ && python3 run.py zk_4203.txt"
 vagrant halt test1
-
-./change_cores.sh 8
+cd ../auxiliary_scripts/ && ./change_cores.sh 8 && cd ..
 
 vagrant up test2
 vagrant ssh test2 -c "cd /vagrant/artifact_evaluation/bug_reproduction/ && python3 run.py docker_bugs.txt"
--- artifact_evaluation/tracing_overhead/setup_redis.sh
+++ artifact_evaluation/tracing_overhead/setup_redis.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+export NEEDRESTART_MODE=a
+
+APT_YES=(-y)
+DPKG_NONINTERACTIVE=(
+  -o Dpkg::Use-Pty=0
+  -o Dpkg::Options::=--force-confdef
+  -o Dpkg::Options::=--force-confold
+)
+
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-sudo apt-get -y install redis-tools jq redis
-sudo apt-get -y update
-sudo apt install -y git maven ant vim openjdk-8-jdk
+sudo -E apt-get install "${APT_YES[@]}" "${DPKG_NONINTERACTIVE[@]}" redis-tools jq redis
+sudo -E apt-get update -y update
+sudo -E apt-get install "${APT_YES[@]}" "${DPKG_NONINTERACTIVE[@]}" git maven ant vim openjdk-8-jdk
 sudo update-alternatives --set java $(sudo update-alternatives --list java | grep "java-8")
-sudo apt-get -y upgrade
+sudo -E apt-get upgrade "${APT_YES[@]}" "${DPKG_NONINTERACTIVE[@]}"
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 echo export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 >> ~/.bashrc
