9b3cbce6e28e32008058df5b0ba8e250e0f7ad0c
Sebastião Amaro <sebastiao.amaro@tecnico.ulisboa.pt>
2026-02-12 07:18:16 +0000

    deleted garbage, fixed scripts

--- artifact_evaluation/prepare_vms.sh
+++ artifact_evaluation/prepare_vms.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 mkdir ~/shared/
-cd scripts/
-./setup_anduril_vm.sh
-cd artifact_evaluation/scripts/
-./setup_docker_vm.sh
-cd artifact_evaluation/scripts/
-./setup_lxc_vm.sh
+cd scripts/ && ./setup_anduril_vm.sh
+cd artifact_evaluation/scripts/ && ./setup_docker_vm.sh
+cd artifact_evaluation/scripts/ && ./setup_lxc_vm.sh
