7f4efaa52e363712916705c938f3f7d18c98ceac
Sebastião Amaro <sebastiao.amaro@tecnico.ulisboa.pt>
2026-02-12 06:00:34 +0000

    fixed folders which hold files

--- artifact_evaluation/scripts/setup_dependencies.sh
+++ artifact_evaluation/scripts/setup_dependencies.sh
@@ -4,12 +4,3 @@ cd /vagrant/auxiliary_scripts/
 ./install.sh
 cd /vagrant/
 pip3 install -e . --break-system-packages
-mkdir /vagrant/tests/bugdetection/
-mkdir /vagrant/tests/bugdetection/hbase
-mkdir /vagrant/tests/bugdetection/hdfs
-mkdir /vagrant/tests/bugdetection/kafka
-mkdir /vagrant/tests/bugdetection/mongo
-mkdir /vagrant/tests/bugdetection/redis
-mkdir /vagrant/tests/bugdetection/redisraft
-mkdir /vagrant/tests/bugdetection/tendermint
-mkdir /vagrant/tests/bugdetection/zookeeper
