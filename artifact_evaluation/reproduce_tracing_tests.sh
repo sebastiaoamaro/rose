38b45b6706633429f1eec32a4c1854fe01427135
Sebastião Amaro <sebastiao.amaro@tecnico.ulisboa.pt>
2026-02-13 19:15:11 +0000

    script fix

--- artifact_evaluation/reproduce_tracing_tests.sh
+++ artifact_evaluation/reproduce_tracing_tests.sh
@@ -3,5 +3,4 @@ rm ~/shared/test3/*
 vagrant up test3
 vagrant ssh test3 -c "cd /vagrant/artifact_evaluation/tracing_overhead/throughput/ && ./overhead_test.sh 25000000"
 vagrant ssh test3 -c "cd /vagrant/artifact_evaluation/tracing_overhead/trace_size/ && python3 run.py"
-vagrant ssh test3 -c "cd /vagrant/artifact_evaluation/tracing_overhead/ && python3 create_table.py"
 vagrant halt test3 > /dev/null
