c02a17a37d9f365e3c1915553cc85a31e4b9b4b9
Sebastião Amaro <sebastiao.amaro@tecnico.ulisboa.pt>
2026-02-16 16:06:44 +0000

    fixed prepare vms

--- artifact_evaluation/prepare_vms.sh
+++ artifact_evaluation/prepare_vms.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
 mkdir -p ~/shared/
-cd scripts/ && ./setup_anduril_vm.sh
-cd scripts/ && ./setup_docker_vm.sh
-cd scripts/ && ./setup_lxc_vm.sh
+./scripts/setup_anduril_vm.sh
+./scripts/setup_docker_vm.sh
+./scripts/setup_lxc_vm.sh
